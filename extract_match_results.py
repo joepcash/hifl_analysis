c9c01f64d445d40bfb2964b80415022876cb17d4
Joseph Cashell <56300631+joepcash@users.noreply.github.com>
2019-11-11 13:46:17 +0000

    Generate Team List
    Added file to generate list of teams from past league tables.

--- extract_match_results.py
+++ extract_match_results.py
@@ -5,49 +5,58 @@ import time
 from bs4 import BeautifulSoup
 from tabulate import tabulate
 
+# Web address section to identify each season's results.
+seasons = ['212885780/1_226766634','838109370/1_181077792']
+
 results = []
-years = []
-for page in range(1,6):
-    url = 'https://hifl.leaguerepublic.com/l/results/838109370/1_181077792/-1/-1/' + str(page) + '.html'
-    response = requests.get(url)
 
-    # Get HTML from url
-    soup = BeautifulSoup(response.text, 'html.parser')
-    # Find table in HTML
-    if soup.find('table'):
-        table = soup.find('table')
-    else:
-        break
-    table_body = table.find('tbody')
-    rows = table_body.find_all('tr')
-    data = []
+for season in seasons:
+    for page in range(1,6):
+        url = 'https://hifl.leaguerepublic.com/l/results/' + season + '/-1/-1/' + str(page) + '.html'
+        response = requests.get(url)
 
-    # Convert extracted data into a table format
-    for row in rows:
-        cols = row.find_all('td')
-        cols = [ele.text.strip() for ele in cols]
-        data.append([ele for ele in cols if ele])  # Get rid of empty values
-
-    for i in range(len(data)):
-        if "W" in data[i][2]:
-            pass
+        # Get HTML from url
+        soup = BeautifulSoup(response.text, 'html.parser')
+        # Find table in HTML
+        if soup.find('table'):
+            table = soup.find('table')
         else:
-            for j in range(len(data[i])):
-                # Remove formatting characters
-                data[i][j] = data[i][j].replace("\t","").\
-                    replace("\r","").replace("\n"," ",1).replace("\n","")
-            # Remove time from date/time column
-            data[i][0] = data[i][0].split(" ")[0]
-            # Split score into separate columns
-            score = data[i][2]
-            data[i][2] = data[i][3]
-            data[i][3] = score.split(" ")[0]
-            data[i].append(score.split(" ")[2])
-            data[i].append(score.split(" ")[4].split("-")[0])
-            data[i].append(score.split(" ")[4].split("-")[1].replace(")",""))
-            results.append(data[i])
+            break
+        table_body = table.find('tbody')
+        rows = table_body.find_all('tr')
+        data = []
 
-    time.sleep(1)
+        # Convert extracted data into a table format
+        for row in rows:
+            cols = row.find_all('td')
+            cols = [ele.text.strip() for ele in cols]
+            data.append([ele for ele in cols if ele])  # Get rid of empty values
+
+        for i in range(len(data)):
+            if "W" in data[i][2]:
+                pass
+            else:
+                for j in range(len(data[i])):
+                    # Remove formatting characters
+                    data[i][j] = data[i][j].replace("\t","").\
+                        replace("\r","").replace("\n"," ",1).replace("\n","")
+                # Remove time from date/time column
+                data[i][0] = data[i][0].split(" ")[0]
+                # Split score into separate columns
+                score = data[i][2]
+                data[i][2] = data[i][3]
+                data[i][3] = score.split(" ")[0]
+                data[i].append(score.split(" ")[2])
+                if "HT" in score:
+                    data[i].append(score.split(" ")[4].split("-")[0])
+                    data[i].append(score.split(" ")[4].split("-")[1].replace(")",""))
+                else:
+                    # If no half-time score given, leave blank.
+                    data[i].append("")
+                    data[i].append("")
+                results.append(data[i])
+
+        time.sleep(1)
 
 print(tabulate(results))
 
